8767b9f037868548b0d3da9d9c890c1dcb40bb61
Luiz Felipe <felipefelizatti215@gmail.com>
2022-03-31 17:04:42 -0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,10 +10,11 @@ export function Footer() {
       px="52"
       align="center"
       justify="center"
+      spacing="3"
     >
       <Text fontWeight="400" fontSize="0.9rem" color="white">(NOME) 2022 Todos direitos reservados</Text>
 
-      <Text fontWeight="300" fontSize="0.8rem" color="white">
+      <Text fontWeight="300" fontSize="0.7rem" color="white">
         Atenção Nós não compartilhamos seu contato com terceiros.
         O tratamento de dados nesse aspecto <br /> acontece em conformaidade com a Lei de
         Proteção de Dados. Você está seguro(a) e pode se <br /> descadastrar em qualquer momento.
